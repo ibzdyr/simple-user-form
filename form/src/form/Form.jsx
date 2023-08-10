20407eb22a2d85ded60fa1c902f7a167da4e4c04
ibzdyr <ibzdy@softserveinc.com>
2023-08-10 10:28:19 +0300

    UC#8 Display values

--- form/src/form/Form.jsx
+++ form/src/form/Form.jsx
@@ -24,6 +24,7 @@ const Form = ({
 
     return <div>
         <div>
+            <h1>User Data</h1>
             <p>First name: {firstName}</p>
             <p>Last name: {lastName}</p>
             <p>Email: {email}</p>
